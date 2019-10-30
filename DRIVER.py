63a7a5c5535cb39069a708485c1450a302564271
adamcwatts <aldburg@gmail.com>
2019-10-29 21:38:22 -0400

    Improved speed of concentration ODE

--- DRIVER.py
+++ DRIVER.py
@@ -3,7 +3,6 @@ import MODEL_BC_IC as BC_IC  # BC_IC - Boundary and Initial Conditions
 import numpy as np
 import pandas as pd
 from scipy.integrate import solve_ivp
-import matplotlib.pyplot as plt
 from time import time
 
 OPI = BC_IC.ODE_PHYSICS_INPUT
@@ -22,7 +21,7 @@ def fabric_initial_conditions_to_df(fabric_ic: 'temp and relative humidity',
     temp_dict = {}
     for key, value in fabric_ic.items():
         # value = value[0]  # turns 1D array into scalar :TODO might not need this
-        multiplier = np.ones(node_count)  # multiplies values by node count
+        multiplier = np.ones(ODE.node_count)  # multiplies values by node count
 
         if key == 'initial clothing temp':
             temp_dict['initial clothing temp [C]'] = value * multiplier
@@ -49,7 +48,7 @@ def fabric_initial_conditions_to_df(fabric_ic: 'temp and relative humidity',
     temp_dict['thickness [m]'] = node_thickness
 
     df = pd.DataFrame.from_dict(temp_dict)
-    names = [f'Fabric Node {x}' for x in range(node_count)]
+    names = [f'Fabric Node {x}' for x in range(ODE.node_count)]
     df.index = names
     # df.rename(columns={'initial clothing rh': 'Initial clothing rh'}, inplace=True)
     return df
@@ -81,7 +80,7 @@ def ode_plateflux_temp(t, y):
     # ode_params = {}
     # n numbers of nodes + 1 node for boundary at plate
 
-    temp_at_clothing_boundary = y[node_count]  # last element of vector associated with temperatures
+    temp_at_clothing_boundary = y[ODE.node_count]  # last element of vector associated with temperatures
 
     h_radiation = OPI['eps_clothing'] * OPI['sigma'] * (
             temp_at_clothing_boundary + boundary_conditions['air temp [K]']) * (
@@ -91,23 +90,23 @@ def ode_plateflux_temp(t, y):
 
     r_rad_convect = 1 / (h_radiation + OPI['h_convection'])
 
-    delta_x = df_fabric_initial_conditions['thickness [m]'].to_numpy()
+    # delta_x = df_fabric_initial_conditions['thickness [m]'].to_numpy()
 
     K = df_wet_fabric['wet fabric thermal conductivity [W/mk]'].to_numpy()
     C = df_wet_fabric['wet fabric specific heat [J/kg K]'].to_numpy()
     P = df_wet_fabric['wet fabric density [kg/m^3]'].to_numpy()
-    U = K / delta_x
+    U = K / ODE.delta_x
     U[-1] = 1 / (r_rad_convect + 0.5 * (
-            delta_x[-1] / K[-1]))  # corrects last element for radiation, convection heat loss
+            ODE.delta_x[-1] / K[-1]))  # corrects last element for radiation, convection heat loss
 
-    material = 1 / (delta_x * np.multiply(C, P))
+    material = 1 / (ODE.delta_x * np.multiply(C, P))
 
     temperature_array = np.append(y[1:], boundary_conditions['air temp [K]'])
 
     q_dry = np.diff(temperature_array) * -1 * U  # -1 due to diff goes a[i+1] - a[i], I need a[i] - a[i+1]
     q_dry = np.concatenate(([y[0]], q_dry), axis=0)  # adds heat flow from plate
 
-    q_gen = (boundary_conditions['plate temp [K]'] - y[1]) * (K[0] / (delta_x[0] * 0.5))  # flux at plate ODE
+    q_gen = (boundary_conditions['plate temp [K]'] - y[1]) * (K[0] / (ODE.delta_x[0] * 0.5))  # flux at plate ODE
     clothing_dydt = material * np.diff(q_dry) * -1  # -1 due to diff goes a[i+1] - a[i], I need a[i] - a[i+1]
 
     dydt = np.concatenate(([q_gen], clothing_dydt), axis=0)
@@ -126,14 +125,14 @@ def ode_plateflux_temp(t, y):
 
 
 def ode_concentration(t, c):
-    delta_x = df_fabric_initial_conditions['thickness [m]'].to_numpy()  # thickness of each node
-    D_WF = df_fabric_initial_conditions['diffusivity of water though fabric [m^2 /s]'].to_numpy()
-
-    delta_path = np.copy(delta_x)  # copy array to create actual path between nodes
-    delta_path[0] = delta_path[0] * 0.5 + OPI['membrane_air_length_equiv']  # node next to hot plate
-    delta_path[-1] = delta_path[-1] * 0.5 + OPI['length_still_air']
-
-    material = D_WF / delta_x
+    # delta_x = df_fabric_initial_conditions['thickness [m]'].to_numpy()  # thickness of each node
+    # D_WF = df_fabric_initial_conditions['diffusivity of water though fabric [m^2 /s]'].to_numpy()
+    #
+    # delta_path = np.copy(delta_x)  # copy array to create actual path between nodes
+    # delta_path[0] = delta_path[0] * 0.5 + OPI['membrane_air_length_equiv']  # node next to hot plate
+    # delta_path[-1] = delta_path[-1] * 0.5 + OPI['length_still_air']
+    #
+    # material = D_WF / delta_x
 
     concentration_array = np.concatenate(
         ([boundary_conditions['water concentration at plate [g/m^3]']], c,
@@ -145,7 +144,7 @@ def ode_concentration(t, c):
     # dcdt_1 = material[1] * (delta_c[1] / delta_x[1] - (delta_c[2]) / delta_x[1])
     # dcdt_2 = material[2] * (delta_c[2] / delta_x[2] - delta_c[3] / (delta_x[2] * 0.5 + still_air_length))
 
-    dcdt = material * (delta_c[0:-1] / delta_path - delta_c[1:] / delta_path)
+    dcdt = ODE.material_concentration_stiffness * (delta_c[0:-1] / ODE.delta_path - delta_c[1:] / ODE.delta_path)
 
     return dcdt
 
@@ -155,12 +154,13 @@ def solution_to_df(ode_sol, solution_type):
 
     if solution_type == 't':
 
-        fabric_solutions = {f'Temp at Node {i + 1} [C]': ode_sol.y[i + 1] - 273.15 for i in range(node_count)}
+        fabric_solutions = {f'Temp at Node {i + 1} [C]': ode_sol.y[i + 1] - 273.15 for i in range(ODE.node_count)}
         fabric_solutions['Time (Minutes)'] = ode_sol.t / 60
         fabric_solutions['Hot Plate Flux'] = ode_sol.y[0]
 
     elif solution_type == 'c':
-        fabric_solutions = {f'Concentration at Node {i} [g/m^3]': ode_sol.y[i] for i in range(node_count)}
+        fabric_solutions = {f'Concentration at Node {i} [g/m^3]': ode_sol.y[i] for i in range(ODE.node_count)}
+        fabric_solutions['Time (Minutes)'] = ode_sol.t / 60
 
     index_names = np.ndarray.tolist(ode_sol.t)
     index_names = [str(name) + " [s]" for name in index_names]
@@ -170,10 +170,58 @@ def solution_to_df(ode_sol, solution_type):
 
     if solution_type == 't':
         sol_df = sol_df[sol_df.columns.tolist()[-2:] + sol_df.columns.tolist()[:-2]]  # reorder last 2 as first 2
+    elif solution_type == 'c':
+        sol_df = sol_df[[sol_df.columns.tolist()[-1]] + sol_df.columns.tolist()[:-1]]
 
     sol_df.index = index_names  # add index names
 
     return sol_df
+
+
+class SolutionSetup:
+    def __init__(self):
+        self.node_count = BC_IC.NUMBER_OF_NODES
+
+        node_tuple = (n, self.node_count)
+        zeros_array = np.zeros(node_tuple)
+
+        self.temps = np.zeros(node_tuple)  # [kelvin]
+        self.raw_water_concentration = zeros_array  # [(g/m^3]  NEED TO VERIFY
+        self.condensation_concentration_history = zeros_array  # [(g/m^3]  NEED TO VERIFY
+        self.fiber_water_concentration_history = zeros_array  # [(g/m^3]  NEED TO VERIFY
+
+        self.enthalpy_sorption_history = zeros_array
+
+        # could condense this into a 3D array time X nodes X type of heat flow
+        self.q_sorption = zeros_array
+        self.q_condensation = zeros_array
+
+        self.q_evaporation = zeros_array
+        self.q_evaporation_history = zeros_array
+
+        self.q_1_history = zeros_array
+        self.q_L_history = zeros_array
+
+        self.relative_humidity_post_absorption_history = zeros_array
+
+        # final_temps_and_concentration = zeros_array
+
+        self.heat_flows_history = np.zeros((n, 1 + self.node_count))
+
+        self.delta_x = None
+        self.D_WF = None
+        self.material_concentration_stiffness = None
+        self.delta_path = None
+
+    def concentration_constants(self, fabric_initial_conditions_data_frame):
+        self.delta_x = fabric_initial_conditions_data_frame['thickness [m]'].to_numpy()  # thickness of each node
+        self.D_WF = fabric_initial_conditions_data_frame['diffusivity of water though fabric [m^2 /s]'].to_numpy()
+
+        self.delta_path = np.copy(self.delta_x)  # copy array to create actual path between nodes
+        self.delta_path[0] = self.delta_path[0] * 0.5 + OPI['membrane_air_length_equiv']  # node next to hot plate
+        self.delta_path[-1] = self.delta_path[-1] * 0.5 + OPI['length_still_air']
+
+        self.material_concentration_stiffness = self.D_WF / self.delta_x
 
 
 if __name__ == '__main__':
@@ -183,36 +231,11 @@ if __name__ == '__main__':
 
     tspan = np.arange(start, finish_seconds + time_step, time_step)
     n = tspan.shape[0]
-    node_count = BC_IC.NUMBER_OF_NODES
 
-    node_tuple = (n, node_count)
-    zeros_array = np.zeros(node_tuple)
-
-    temps = np.zeros(node_tuple)  # [kelvin]
-    raw_water_concentration = zeros_array  # [(g/m^3]  NEED TO VERIFY
-    condensation_concentration_history = zeros_array  # [(g/m^3]  NEED TO VERIFY
-    fiber_water_concentration_history = zeros_array  # [(g/m^3]  NEED TO VERIFY
-
-    enthalpy_sorption_history = zeros_array
-
-    # could condense this into a 3D array time X nodes X type of heat flow
-    q_sorption = zeros_array
-    q_condensation = zeros_array
-
-    q_evaporation = zeros_array
-    q_evaporation_history = zeros_array
-
-    q_1_history = zeros_array
-    q_L_history = zeros_array
-
-    relative_humidity_post_absorption_history = zeros_array
-
-    final_temps_and_concentration = zeros_array
-
-    heat_flows_history = np.zeros((n, 1 + node_count))
+    ODE = SolutionSetup()  # initialize ODE arrays and key parameters
 
     fabric_data = functions.fabric_parameters(BC_IC.FABRIC_INPUT_PARAMETERS)
-    fabric_node_dimensions = functions.fabric_1D_meshing(fabric_data, node_count)
+    fabric_node_dimensions = functions.fabric_1D_meshing(fabric_data, ODE.node_count)
 
     boundary_conditions = boundary_conditions_with_kelvin(BC_IC.BOUNDARY_INPUT_PARAMETERS)
     fix_floats_to_numpy_array = convert_float_to_array(BC_IC.FABRIC_IC_INPUT)
@@ -223,6 +246,7 @@ if __name__ == '__main__':
     df_wet_fabric = functions.wet_fabric_calc(df_fabric_initial_conditions,
                                               df_fabric_initial_conditions['initial clothing rh'])
 
+    ODE.concentration_constants(df_fabric_initial_conditions)  # precalculute constants for concentration ODE
     # TEST RH Equilibrium Function
 
     # temps_c = np.array([20, 30, 35, 40])
